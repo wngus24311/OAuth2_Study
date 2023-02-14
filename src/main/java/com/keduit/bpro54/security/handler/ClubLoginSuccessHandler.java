5651e2ba6b4c00b1d647679463148cff531e4094
wngus24311 <wngus24311@gmail.com>
2023-02-14 11:20:14 +0900

    first Commit 미완성

--- src/main/java/com/keduit/bpro54/security/handler/ClubLoginSuccessHandler.java
+++ src/main/java/com/keduit/bpro54/security/handler/ClubLoginSuccessHandler.java
@@ -1,7 +1,10 @@
 package com.keduit.bpro54.security.handler;
 
+import com.keduit.bpro54.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.ServletException;
@@ -11,9 +14,21 @@ import java.io.IOException;
 
 @Log4j2
 public class ClubLoginSuccessHandler implements AuthenticationSuccessHandler {
+    private RedirectStrategy redirectStrategy;
+    private PasswordEncoder passwordEncoder;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("==============================");
         log.info("onAuthenticationSuccess");
+
+        ClubAuthMemberDTO authMemberDTO = (ClubAuthMemberDTO) authentication.getPrincipal();
+
+        boolean fromSocial = authMemberDTO.isFromSocial();
+
+        boolean passwordResult = passwordEncoder.matches("1111", authMemberDTO.getPassword());
+
+        if (fromSocial && passwordResult) {
+            redirectStrategy.sendRedirect(request, response, "/sample/modify?fromSocial");
+        }
     }
 }
